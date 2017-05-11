ee863569208470ee5af321fb3d026fee1f56ca93
Sercan <sercancayir42@gmail.com>
2017-05-11 11:33:36 +0300

    ilk commit

---
+++ VSApp/VSApp.cpp
@@ -0,0 +1,16 @@
+// VSApp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	cout << "Ilk kod " << endl;
+	return 0;
+
+}
+
